a5aad33501b6b0dc3849346698e543b412531f6c
Jobayer-ak <jobayer.ak@gmail.com>
2022-05-27 12:07:13 +0600

    useparams() problem solved

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -22,13 +22,22 @@ async function run() {
     await client.connect();
     const partsCollection = client.db("akAccessories").collection("parts");
 
-    // get all parts
+    // parts api
     app.get("/parts", async (req, res) => {
       const query = {};
       const cursor = partsCollection.find(query);
       const parts = await cursor.toArray();
       console.log(parts);
       res.send(parts);
+    });
+
+    // GET specific parts
+    app.get("/parts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const item = await partsCollection.findOne(query);
+
+      res.send(item);
     });
   } finally {
   }
