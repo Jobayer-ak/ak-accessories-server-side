6bd527d38c194c0d69018c60b7785f8a793336f7
Jobayer-ak <jobayer.ak@gmail.com>
2022-05-30 23:06:52 +0600

    done

--- index.js
+++ index.js
@@ -155,7 +155,7 @@ async function run() {
     });
 
     // GET specific parts
-    app.get("/parts/:id", verifyJWT, verifyAdmin, async (req, res) => {
+    app.get("/parts/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const item = await partsCollection.findOne(query);
